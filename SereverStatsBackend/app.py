024d726cf1e2bdca653c5d302af140311fbb3673
Anmol <eranmolgurjar@gmail.com>
2024-11-15 00:58:39 +0530

    Created New API for gettting initial chart data

--- SereverStatsBackend/app.py
+++ SereverStatsBackend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO, emit
 import psutil
 import time
@@ -7,13 +7,31 @@ from datetime import datetime
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+cpu_uses = []
+memory_uses = []
+timestampcoll = []
+
+@app.route('/api/lastmin-stats', methods=['GET'])
+def lastmin_stats():
+    return jsonify({"cpu": cpu_uses, "memory":memory_uses,'timstamp':timestampcoll})
+
+def resultshortner(arr):
+    if len(arr) > 60:
+        arr.pop(0)
+    return arr
 def get_server_stats():
     while True:
         memory = psutil.virtual_memory().percent
+        memory_uses.append(memory)
+        resultshortner(memory_uses)
         cpu = psutil.cpu_percent(interval=0.5)
+        cpu_uses.append(cpu)
+        resultshortner(cpu_uses)
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        timestampcoll.append(timestamp)
+        resultshortner(timestampcoll)
         socketio.emit('update_stats', {'cpu': cpu, 'memory': memory, 'timstamp':timestamp})
-        #time.sleep(1)
+        time.sleep(0.5)
 
 @app.route('/')
 def index():
